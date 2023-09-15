637f2971e562597233751828f02c3acace926b6f
Alvaro Muñoz <pwntester@github.com>
2023-09-15 13:43:18 +0200

    Fix missing packs in pr-suites-packs.sh

--- .github/scripts/pr-suites-packs.sh
+++ .github/scripts/pr-suites-packs.sh
@@ -27,21 +27,44 @@ for file in $(gh pr view "$PR_NUMBER" --json files --jq '.files.[].path'); do
         codeql pack create "$LANGUAGE"
 
         # if the version of the pack is changed, comment in the PR
-        PUBLISHED_VERSION=$(gh api /orgs/githubsecuritylab/packages/container/codeql-"$LANGUAGE"/versions --jq '.[0].metadata.container.tags[0]')
-        CURRENT_VERSION=$(grep version "$LANGUAGE"/qlpack.yml | awk '{print $2}')
+        PUBLISHED_VERSION=$(gh api /orgs/githubsecuritylab/packages/container/codeql-"$LANGUAGE"-queries/versions --jq '.[0].metadata.container.tags[0]')
+        CURRENT_VERSION=$(grep version "$LANGUAGE"/src/qlpack.yml | awk '{print $2}')
 
         if [ "$PUBLISHED_VERSION" != "$CURRENT_VERSION" ]; then
             echo "[+] New version of pack detected: $PUBLISHED_VERSION (pub) != $CURRENT_VERSION (cur)"
-
-            comment="New version of pack \`githubsecuritylab/codeql-$LANGUAGE\` will be created on merge: \`$PUBLISHED_VERSION\`->\`$CURRENT_VERSION\`"
-
+            comment="New version of pack \`githubsecuritylab/codeql-$LANGUAGE-queries\` will be created on merge: \`$PUBLISHED_VERSION\`->\`$CURRENT_VERSION\`"
             if [[ ! $(gh pr view "$PR_NUMBER" --json comments --jq '.comments.[].body' | grep "$comment") ]]; then
                 echo "[+] Commenting on PR"
                 gh pr comment "$PR_NUMBER" \
                     --body "$comment"
-
             fi
+        fi
 
+        # Same for the libs pack
+        PUBLISHED_VERSION=$(gh api /orgs/githubsecuritylab/packages/container/codeql-"$LANGUAGE"-libs/versions --jq '.[0].metadata.container.tags[0]')
+        CURRENT_VERSION=$(grep version "$LANGUAGE"/lib/qlpack.yml | awk '{print $2}')
+
+        if [ "$PUBLISHED_VERSION" != "$CURRENT_VERSION" ]; then
+            echo "[+] New version of pack detected: $PUBLISHED_VERSION (pub) != $CURRENT_VERSION (cur)"
+            comment="New version of pack \`githubsecuritylab/codeql-$LANGUAGE-libs\` will be created on merge: \`$PUBLISHED_VERSION\`->\`$CURRENT_VERSION\`"
+            if [[ ! $(gh pr view "$PR_NUMBER" --json comments --jq '.comments.[].body' | grep "$comment") ]]; then
+                echo "[+] Commenting on PR"
+                gh pr comment "$PR_NUMBER" \
+                    --body "$comment"
+            fi
+        fi
+        # Same for the libs extensions pack
+        PUBLISHED_VERSION=$(gh api /orgs/githubsecuritylab/packages/container/codeql-"$LANGUAGE"-extensions/versions --jq '.[0].metadata.container.tags[0]')
+        CURRENT_VERSION=$(grep version "$LANGUAGE"/ext/qlpack.yml | awk '{print $2}')
+
+        if [ "$PUBLISHED_VERSION" != "$CURRENT_VERSION" ]; then
+            echo "[+] New version of pack detected: $PUBLISHED_VERSION (pub) != $CURRENT_VERSION (cur)"
+            comment="New version of pack \`githubsecuritylab/codeql-$LANGUAGE-extensions\` will be created on merge: \`$PUBLISHED_VERSION\`->\`$CURRENT_VERSION\`"
+            if [[ ! $(gh pr view "$PR_NUMBER" --json comments --jq '.comments.[].body' | grep "$comment") ]]; then
+                echo "[+] Commenting on PR"
+                gh pr comment "$PR_NUMBER" \
+                    --body "$comment"
+            fi
         fi
         
         PACK_COMPILED=true
