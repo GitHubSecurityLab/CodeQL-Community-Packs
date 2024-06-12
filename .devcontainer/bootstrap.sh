68aea5dadd9d5a51e5f4117693343392837d590c
Mathew Payne <2772944+GeekMasher@users.noreply.github.com>
2024-06-12 13:36:24 +0000

    fix: Update bootstrap to fix issues

--- .devcontainer/bootstrap.sh
+++ .devcontainer/bootstrap.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
 # Assumes CodeQL is installed via the VSCode Plugin
-CODEQL_CODESPACES_PATH="/root/.vscode-remote/data/User/globalStorage/github.vscode-codeql/distribution1/codeql/codeql"
-
-if [ -d "$CODEQL_CODESPACES_PATH" ]; then
-  export PATH=$CODEQL_CODESPACES_PATH:$PATH
-
-else
-  # Installs CodeQL CLI via GH extension
-  gh extensions install github/gh-codeql
-  gh codeql install-stub
-
-fi
+CODEQL_CODESPACES_PATH="/root/.vscode-remote/data/User/globalStorage/github.vscode-codeql/distribution1/codeql"
+export PATH=$CODEQL_CODESPACES_PATH:$PATH
 
 # Clone an instance of the CodeQL repository
 if [ ! -d "./codeql" ]; then
-  git clone depth=1 https://github.com/github/codeql ./codeql
-else
+  git clone --depth=1 https://github.com/github/codeql ./codeql
+fi
