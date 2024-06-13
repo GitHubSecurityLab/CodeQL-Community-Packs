9b93f9e1cc9cd39ffb04028fcb6c3a53a269b0e7
Mathew Payne <2772944+GeekMasher@users.noreply.github.com>
2024-06-13 13:01:22 +0000

    feat: Add better dev container support

--- .devcontainer/bootstrap.sh
+++ .devcontainer/bootstrap.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+set -e
 
-# Install CodeQL Stub
-cp ./.devcontainer/codeql.sh /usr/local/bin/codeql
+echo "Installing GH Extensions..."
+gh extensions install GitHubSecurityLab/gh-mrva
+gh extensions install advanced-security/gh-codeql-scan
+
+echo "Installing stubs..."
+chmod +x .devcontainer/scripts/* && cp -r .devcontainer/scripts/* /usr/local/bin/
 
 # Clone an instance of the CodeQL repository
+# https://github.com/github/codeql/tree/codeql-cli/latest
+echo "Cloning CodeQL repository..."
 if [ ! -d "./codeql" ]; then
-  git clone --depth=1 https://github.com/github/codeql ./codeql
+  git clone \
+    --branch codeql-cli/latest \
+    https://github.com/github/codeql ./codeql
 fi
